d912446c69af454b187f703cb3ebf534e188aa92
nghiach <chnghia@gmail.com>
2021-03-01 14:47:49 +0700

    update simple_io gpu

--- build_simple_io_gpu.sh
+++ build_simple_io_gpu.sh
@@ -1,2 +1,2 @@
-bazel build -c opt --define MEDIAPIPE_DISABLE_GPU=1 \
-    myMediapipe/examples/desktop/simple_io:simple_io_gpu
+bazel build -c opt --copt -DMESA_EGL_NO_X11_HEADERS --copt -DEGL_NO_X11 \
+    myMediapipe/examples/desktop/simple_io:simple_io_demo_gpu
