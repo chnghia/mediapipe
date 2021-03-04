8b479f60d4867e5455c7f9202399e0d34f1c1ab3
nghiach <chnghia@gmail.com>
2021-03-04 10:14:31 +0700

    add python mqtt client

--- python_mqtt/MQTT_subscribe.py
+++ python_mqtt/MQTT_subscribe.py
@@ -18,10 +18,9 @@ client = mqtt.Client("Client")
 client.username_pw_set("nghia", "123456")
 client.connect(mqttBroker) 
 
-client.loop_start()
+# client.loop_start()
 
 client.subscribe("handCommander/tv/ir_command")
 client.on_message=on_message 
 
-time.sleep(30)
-client.loop_stop()
+client.loop_forever()
